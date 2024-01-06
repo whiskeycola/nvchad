2d5fb194d9f712f9105cb86815d625011b2e6d8d
geniusmerely <dima.tlens@gmail.com>
2024-01-06 04:11:10 +0200

    add support rust in tailwind

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,10 +4,10 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "cssls", "clangd", "tailwindcss" }
+local servers = { "cssls", "clangd" }
 
 for _, lsp in ipairs(servers) do
-lspconfig[lsp].setup {
+  lspconfig[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
   }
@@ -16,4 +16,64 @@ lspconfig.html.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = { "html", "handlebars" },
+}
+lspconfig.tailwindcss.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  init_options = {
+    userLanguages = {
+      rust = "html",
+    },
+  },
+  filetypes = {
+    "aspnetcorerazor",
+    "astro",
+    "astro-markdown",
+    "blade",
+    "clojure",
+    "django-html",
+    "htmldjango",
+    "edge",
+    "eelixir",
+    "elixir",
+    "ejs",
+    "erb",
+    "eruby",
+    "gohtml",
+    "gohtmltmpl",
+    "haml",
+    "handlebars",
+    "hbs",
+    "html",
+    "html-eex",
+    "heex",
+    "jade",
+    "leaf",
+    "liquid",
+    "markdown",
+    "mdx",
+    "mustache",
+    "njk",
+    "nunjucks",
+    "php",
+    "razor",
+    "slim",
+    "twig",
+    "css",
+    "less",
+    "postcss",
+    "sass",
+    "scss",
+    "stylus",
+    "sugarss",
+    "javascript",
+    "javascriptreact",
+    "reason",
+    "rescript",
+    "typescript",
+    "typescriptreact",
+    "vue",
+    "svelte",
+    "rust",
+  },
 }
